28f647fdec878380b3727d1639d3bde6e5efaa31
jaywkim97 <rarangoon@gmail.com>
2024-07-09 16:56:02 +0900

    [Bronze III] Title: 최소, 최대, Time: 520 ms, Memory: 89264 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,31 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arr = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int min = arr[0]; // 배열의 첫 번째 요소를 최소값으로 초기화
-        int max = arr[0]; // 배열의 첫 번째 요소를 최대값으로 초기화
+        Integer.parseInt(br.readLine()); // 첫 줄 N 은 안쓰이므로 입력만 받는다.
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 1; i < arr.length; i++) { // 배열의 두 번째 요소부터 순회
-            if (arr[i] < min) { // 현재 요소가 최소값보다 작으면
-                min = arr[i]; // 최소값을 현재 요소로 갱신
+        int max = -1000001;
+        int min = 1000001;
+
+        while (st.hasMoreTokens()) {
+            int val = Integer.parseInt(st.nextToken());
+            if (val > max) {
+                max = val;
             }
-            if (arr[i] > max) { // 현재 요소가 최대값보다 크면
-                max = arr[i]; // 최대값을 현재 요소로 갱신
+            if (val < min) {
+                min = val;
             }
         }
-
-        System.out.println(min + " " + max); // 최소값 출력
-
+        System.out.println(min + " " + max);
     }
 }
-
-
