827c6c73a129aada596cffafab13f261e7b0a508
jaywkim97 <rarangoon@gmail.com>
2024-08-22 15:59:18 +0900

    [level 0] Title: 암호 해독, Time: 0.18 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder result = new StringBuilder();
+        for (int i = code - 1; i < cipher.length(); i += code) {
+            result.append(cipher.charAt(i));
+        }
+        return result.toString();
+    }
+}
