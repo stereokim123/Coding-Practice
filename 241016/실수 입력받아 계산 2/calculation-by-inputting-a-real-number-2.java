6b3de7f3a79cdd1dc6d5fa0e3ba63bee9299cecf
jaywkim97 <rarangoon@gmail.com>
2024-10-16 16:58:43 +0900

    Title: 실수 입력받아 계산 2, Time: 101ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241016/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+
+        double result = a + 1.5;
+        System.out.printf("%.3f", result);
+    }
+}
