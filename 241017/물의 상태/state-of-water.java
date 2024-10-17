6d5054d3fade04a7e2c3ce4e36a36e717a0db708
jaywkim97 <rarangoon@gmail.com>
2024-10-17 20:47:01 +0900

    Title: 물의 상태, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241017/물의 상태/state-of-water.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    String answer = "";
+    if (n < 0) {
+      answer = "ice";
+    } else if (n >= 100){
+      answer = "vapor";
+    } else {
+      answer = "ice";
+    }
+
+    System.out.println(answer);
+
+  }
+}
