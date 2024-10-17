39b32b804c6c890e77d148fca0357c6d5bad6b17
jaywkim97 <rarangoon@gmail.com>
2024-10-17 20:54:19 +0900

    Title: 살 수 있는 물건, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241017/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    String answer = "";
+
+    if (n >= 3000) {
+        answer = "book";
+    } else if (n >= 1000){
+        answer = "mask";
+    } else{
+        answer = "no";
+    }
+
+    System.out.println(answer);
+
+  }
+}
