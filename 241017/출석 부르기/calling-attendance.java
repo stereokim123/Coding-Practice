19e47e9d78f1d3561da1db1c60df89b32e1c23b1
jaywkim97 <rarangoon@gmail.com>
2024-10-17 23:24:56 +0900

    Title: 출석 부르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241017/출석 부르기/calling-attendance.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    String answer = "";
+    if (n = 1) {
+      answer = "John";
+    } else if (n = 2){
+      answer = "Tom";
+    } else if (n = 3) {
+      answer = "paul";
+    } else {
+        answer = "Vacancy";
+    }
+
+    System.out.println(answer);
+
+  }
+}
