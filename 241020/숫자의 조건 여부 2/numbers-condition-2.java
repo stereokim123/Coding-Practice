017946862725ea89a11f5e97c65d6683700f7a8b
jaywkim97 <rarangoon@gmail.com>
2024-10-20 13:55:37 +0900

    Title: 숫자의 조건 여부 2, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241020/숫자의 조건 여부 2/numbers-condition-2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        if (a == 5) {
+            System.out.println("A");
+        }
+        if (a / 2 == 0) {
+            System.out.println("B");
+        } else{
+            System.out.println("");
+        }
+    }
+}
