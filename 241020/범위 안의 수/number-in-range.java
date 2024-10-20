c37ad3af79aab4d9c41d413b0530d32cf621687b
jaywkim97 <rarangoon@gmail.com>
2024-10-20 19:07:33 +0900

    Title: 범위 안의 수, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 241020/범위 안의 수/number-in-range.java
+++ 241020/범위 안의 수/number-in-range.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = br.readLine();
+        int a = Integer.parseInt(br.readLine());
 
         if (a >= 10 && a <= 20) {
             System.out.println("yes");
