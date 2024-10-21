28d0003ce5250ad1c1e2e3147254a0770bdc714a
jaywkim97 <rarangoon@gmail.com>
2024-10-21 15:29:10 +0900

    Title: 특정 조건 세 정수 비교, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241021/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+        int answer1;
+        int answer2;
+
+        if (a == b && b < c) {
+            answer1 = 1;
+        } else {
+            answer1 = 0;
+        }
+
+        if (a == c && c < b) {
+            answer1 = 1;
+        } else {
+            answer1 = 0;
+        }
+
+        if (a == b && a == c) {
+            answer2 = 1;
+        } else {
+            answer2 = 0;
+        }
+
+        System.out.println(answer1 + " " + answer2);
+
+    }
+}
